06d9e3a0f6a9d2f53afed974bbe31112b45da4fe
= <=>
2024-02-10 00:53:26 +0500

    Org code

--- app.js
+++ app.js
@@ -1,47 +1,132 @@
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
-const { Telegraf } = require("telegraf");
+const { Telegraf, Input } = require("telegraf");
 const { message } = require("telegraf/filters");
+const cron = require("node-cron");
+const config = require("config");
 
-// чтение папки
-fs.readdir("files", (error, files) => {
-  console.log("Запущен обработчик файлов...");
-  // проверка, есть ли ошибка чтения
-  if (error) return console.log(error);
+// токен бота
+const token = config.get("telegram.token");
+// идентификатор чата или канала
+const chatId = config.get("telegram.chatId");
+// путь к папке с файлами
+const filePath = config.get("filePath");
+// расписание
+const jobSchedule = config.get("cron");
+// режим запуска
+const mode = config.get("mode");
 
-  const op = files.map((name) => ({
-    name: name.split("-")[0],
-    filePath: "files/" + name,
-  }));
-
-  const groupFiles = _.groupBy(files, (name) => name.split("-")[0]);
-
-  Object.keys(groupFiles).forEach((key) => {
-    const msg = groupFiles[key];
-    pushPost(`files/${msg[0]}`);
-  });
-
-  console.log("Обработка файлов завершена....");
-});
-
-const token = "6516029694:AAFcm3HRJn9qWza93THo3jfnNoiF4aNAtZQ";
+// экземпляр бота
 const bot = new Telegraf(token);
-const CHANNEL_ID = -1001693693655;
 
-//bot.launch();
+/**
+ * Запуск отправки постов в чат или канал
+ */
+const run = () => {
+  // чтение папки
+  fs.readdir(filePath, (error, files) => {
+    console.log("Запущен обработчик файлов...");
 
-const pushPost = (filePath) => {
-  fs.readFile(filePath, function (error, data) {
-    if (error) {
-      // если возникла ошибка
-      return console.log(error);
-    }
-    //console.log(data);
+    if (!files && files.length <= 0)
+      console.log("По указанному пути нет постов");
+
+    // проверка, есть ли ошибка чтения
+    if (error) return console.log(error);
+
+    // сгруппированный по постам массив файлов
+    const groupFiles = _.groupBy(files, (name) => name.split("-")[0]);
+
+    // вывод в консоль количество постов в папке
+    console.log(`Найдено постов: ${Object.keys(groupFiles).length}`);
+
+    // перебора постов
+    Object.keys(groupFiles).forEach((key) => {
+      // список файлов поста
+      const msgFiles = groupFiles[key];
+      // текстовое сообщение
+      const msg = msgFiles[0];
+      // изображения
+      const images = msgFiles.filter((f) => f.includes("jpg"));
+      // видео
+      const videos = msgFiles.filter((f) => f.includes("mp4"));
+
+      pushImages(images, msg);
+      pushVideos(videos);
+    });
+
+    console.log("Обработка файлов завершена....");
+  });
+};
+
+/**
+ * Отправка сообщения
+ * @param {string} file
+ */
+const pushPost = (file) => {
+  fs.readFile(file, function (error, data) {
+    if (error) return console.log(error);
+
     // выводим считанные данные
     if (data) bot.telegram.sendMessage(CHANNEL_ID, data.toString());
   });
 };
+
+/**
+ * Отправка изображений в канал
+ * @param {Array<string>} images
+ * @param {string} msg
+ */
+const pushImages = (images, msg) => {
+  if (!images || images.length === 0) return;
+
+  fs.readFile(getFilePath(msg), (error, data) => {
+    if (error) return console.log(error);
+
+    let index = 0;
+
+    images
+      .sort((a, b) => a.localeCompare(b))
+      .forEach((image) => {
+        const url = getFilePath(image);
+        bot.telegram.sendPhoto(chatId, Input.fromLocalFile(url), {
+          caption: index === 0 ? data.toString() : ""
+        });
+        index++;
+      });
+  });
+};
+
+/**
+ * Отправка видео в канал
+ * @param {Array<string>} videos
+ */
+const pushVideos = (videos) => {
+  if (!videos || videos.length === 0) return;
+  videos.forEach((video) => {
+    const url = getFilePath(video);
+    const videoPath = Input.fromReadableStream(fs.createReadStream(url));
+    bot.telegram.sendPhoto(CHANNEL_ID, videoPath);
+  });
+};
+
+/**
+ * Получить полный путь к файлу
+ * @param {string} file
+ * @returns string
+ */
+const getFilePath = (file) => {
+  return file ? `./${filePath}/${file}` : null;
+};
+
+// режим запуска
+// 'mode' = 0, обычный запуска,
+// любое другое значние, через крон
+if (mode === 0) {
+  run();
+} else {
+  cron.schedule(jobSchedule, run);
+}
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
